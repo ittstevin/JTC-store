523a2cfa9b419d3664baf6de7321736c1f238016
ittstevin <tevingichoya@gmail.com>
2024-02-19 14:17:06 +0300

    project started

---
+++ app/components/nav/Navbar.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import Container from "../Container";
+
+const Navbar = () => {
+    return (
+    <div className="
+    sticky
+    top-0 
+    w-full 
+    bg-slate-200 
+    z-30 
+    shadow-sm
+    ">
+        <div className="py-4 border-b-[1px]">
+            <Container>
+                <div className="
+                flex
+                items-center
+                justify-between
+                gap-3
+                md:gap-0
+                ">
+                    <Link href="/">
+                        JTC-STORE
+                    </Link>
+                    <div className="hidden md:block">search</div>
+                    <div className="
+                    flex
+                    items-center
+                    gap-8
+                    md:gap-12
+                    ">
+                        <div>CartCount</div>
+                        <div>UserMenu</div>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    </div>
+    );
+}
+ 
+export default Navbar;
---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <div>JTC-Store</div>
+  )
+}

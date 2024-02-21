6dc3a32e59d466152ced81adbed4c42ad513db0b
ittstevin <tevingichoya@gmail.com>
2024-02-21 13:25:10 +0300

    added review section but without avatar

---
+++ app/product/[productId]/ListRating.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import Heading from "@/app/components/Heading";
+import { Rating } from "@mui/material";
+import moment from "moment";
+
+interface ListRatingProps{
+    product: any;
+}
+
+const ListRating:React.FC<ListRatingProps> = ({product}) => {
+    return ( 
+        <div>
+            <Heading title="Product Review"/>
+            <div className="text-sm mt-2">
+                {product.reviews && product.reviews.map((Review: any) => {
+                    return(
+                        <div key={Review.id} className="max-w-300px">
+                            <div className="flex gap-2 items-center">
+                                <div>avatar</div>
+                                <div className="font-semibold">{Review?.user.name}</div>
+                                <div className="font-light">{moment(Review.createdDate).fromNow()}</div>
+                            </div>
+                            <div className="mt-2">
+                                <Rating value={Review.rating} readOnly/>
+                                <div className="ml-2">{Review.comment}</div>
+                                <hr className="mt-4 mb-4"/>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+     );
+}
+ 
+export default ListRating;
--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,6 +1,7 @@
 import { product } from "@/utils/product";
 import Container from "@/app/components/Container"
 import ProductDetails from "./ProductDetails";
+import ListRating from "./ListRating";
 
 interface IPrams {
     productId?: string
@@ -13,6 +14,10 @@ const Product = ({params} : {params: IPrams}) => {
         <div className="p-8">
             <Container>
                 <ProductDetails product = {product} />
+                <div className="flex flex-col mt-20 gap-4">
+                    <div>Add Rating</div>
+                    <ListRating product={product}/>
+                </div>
             </Container>
         </div>
      );

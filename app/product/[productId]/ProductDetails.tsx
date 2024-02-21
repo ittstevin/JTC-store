c1eeefcd697d7e52ccf7a8ea2fc36bee4a6481ee
ittstevin <tevingichoya@gmail.com>
2024-02-21 12:54:25 +0300

    updated images on product details

--- app/product/[productId]/ProductDetails.tsx
+++ app/product/[productId]/ProductDetails.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Button from "@/app/components/Button";
+import ProductImage from "@/app/components/products/ProductImage";
 import SetColor from "@/app/components/products/SetColor";
 import SetQuantity from "@/app/components/products/SetQuantity";
 import { Rating } from "@mui/material";
@@ -76,7 +77,11 @@ const Horizontal =() => {
 
     return ( 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-            <div>Images</div>
+            <ProductImage 
+                cartProduct={CartProduct} 
+                product={product} 
+                handleColorSelect={handleColorSelect}/>
+
             <div className="flex flex-col gap-1 text-slate-500 text-sm">
                 <h2 className="text-3xl font-medium text-slate-700">{product.name}</h2>
                 <div className="flex items-center gap-2">
@@ -106,8 +111,8 @@ const Horizontal =() => {
                 handleQtyDecrease={handleQtyDecrease}
                 />
                 <Horizontal />
-                <div>
-                    <Button 
+                <div className="max-w-[300px]">
+                    <Button
                     label="Add To Cart"
                     onClick={() => {}}
                     />

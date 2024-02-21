8409e6f8ec1121fd74f9df97be80e54175d70907
ittstevin <tevingichoya@gmail.com>
2024-02-21 10:26:28 +0300

    improved product details

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,10 +1,21 @@
+import { product } from "@/utils/product";
+import Container from "@/app/components/Container"
+import ProductDetails from "./ProductDetails";
+
 interface IPrams {
     productId?: string
 }
 
 const Product = ({params} : {params: IPrams}) => {
     console.log('params', params);
-    return ( <div>Product page</div> );
+    product
+    return ( 
+        <div className="p-8">
+            <Container>
+                <ProductDetails product = {product} />
+            </Container>
+        </div>
+     );
 }
  
 export default Product;
--- utils/products.tsx
+++ utils/products.tsx
@@ -1,5 +1,3 @@
-//I am changing the image links from firebase to amazon, firebase has issues with these direct links. Please remember to add the amazon link(that will show up at the error) instead of firebase at the cofig for it to work out.
-
 export const products = [
     {
       id: "64a654593e91b8e73a351e9b",

d8804a7fe9f46e52ed6e4bdf32cc1b5d0eb5d134
ittstevin <tevingichoya@gmail.com>
2024-02-22 10:02:46 +0300

    fixed qtyDecrease from the cart

--- hooks/useCart.tsx
+++ hooks/useCart.tsx
@@ -8,6 +8,7 @@ type CartContextType = {
     handleAddProductToCart: (product: CartProductType) => void;
     handleRemoveProductFromCart: (product: CartProductType) => void;
     handleCartQtyIncrease: (product: CartProductType) => void;
+    handleCartQtyDecrease: (product: CartProductType) => void;
 };
 
 export const CartContext = createContext<CartContextType | null>(null);
@@ -74,12 +75,33 @@ export const CartContextProvider = (props: Props) => {
         }
     }, [cartProducts])
 
+    const handleCartQtyDecrease = useCallback((product: CartProductType) => {
+        let updatedCart;
+
+        if(product.quantity === 1){
+            return toast.error('Minimum Reached!')
+        }
+
+        if(cartProducts){
+            updatedCart = [...cartProducts]
+            const existingIndex = cartProducts.findIndex((item) => item.id === product.id);
+
+            if(existingIndex > -1){
+                updatedCart[existingIndex].quantity = --updatedCart[existingIndex].quantity
+            }
+
+            setCartProducts(updatedCart)
+            localStorage.setItem('JTCCartItems', JSON.stringify(updatedCart))
+        }
+    }, [cartProducts])
+
     const value = {
         cartTotalQty,
         cartProducts,
         handleAddProductToCart, 
         handleRemoveProductFromCart,
         handleCartQtyIncrease,
+        handleCartQtyDecrease,
     }
 
     return <CartContext.Provider value={value} {...props}/>
